cbfa5f7bca34a4098aae42793a3aaefad1afef82
guddn1963 <119921477+guddn1963@users.noreply.github.com>
2022-12-26 10:03:37 +0900

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+mysql -u root -pdkagh1. -h mydb -e "create databases django;"
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+mysql -u root -pdkagh1. -h mydb -e "SET collation_connection = ‘utf8_general_ci’;"
+mysql -u root -pdkagh1. -h mydb -e "ALTER DATABASE django CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -u root -pdkagh1. -h mydb -e "use django;" -e "ALTER TABLE pybo_question CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;" -e "ALTER TABLE pybo_reservation CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;"
+python3 manage.py runserver 0.0.0.0:8080
