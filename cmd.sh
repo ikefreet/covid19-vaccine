75812b45054d44b40f03c401a31356a262901897
ikefreet <117957214+ikefreet@users.noreply.github.com>
2022-12-27 10:14:37 +0900

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-mysql -u root -pdkagh1. -h mydb -e "create database django;"
+mysql -u root -h mydb -e "create database django;"
+mysql -u root -h mydb -e "use mysql;" -e "create user 'django'@''%' identified by 'django';"
+mysql -u root -h mydb -e "grant all privileges on django.* to django@'%';" -e "flush privileges;"
 python3 manage.py makemigrations
 python3 manage.py migrate
-mysql -u root -pdkagh1. -h mydb -e "SET collation_connection = ‘utf8_general_ci’;"
-mysql -u root -pdkagh1. -h mydb -e "ALTER DATABASE django CHARACTER SET utf8 COLLATE utf8_general_ci;"
-mysql -u root -pdkagh1. -h mydb -e "use django;" -e "ALTER TABLE pybo_question CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;" -e "ALTER TABLE pybo_reservation CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -u root -h mydb -e "SET collation_connection = ‘utf8_general_ci’;"
+mysql -u root -h mydb -e "ALTER DATABASE django CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -u root -h mydb -e "use django;" -e "ALTER TABLE pybo_question CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;" -e "ALTER TABLE pybo_reservation CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;"
 python3 manage.py runserver 0.0.0.0:8080
