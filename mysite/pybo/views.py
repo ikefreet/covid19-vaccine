6de4e938165584deba33b845c5c98da0a31a65b6
ikefreet <117957214+ikefreet@users.noreply.github.com>
2022-12-27 14:22:35 +0900

    Update views.py

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -8,7 +8,7 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 import pymysql
 
-con = pymysql.connect(host='localhost', user='django', password='django', db='django', charset='utf8')
+con = pymysql.connect(host='mydb', user='django', password='django', db='django', charset='utf8')
 cur = con.cursor()
  
 
